d62bb30a16a67294b6542c471d0251a3b0729205
Gabriel Mateus da Silva <g.silva01@cloud.com>
2024-01-29 20:53:38 -0300

    Incluindo modelos de templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Gabriel Silva',
+    })
 
 def contato(request):
-    return HttpResponse('contato')
+    return HttpResponse('recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('sobre')
