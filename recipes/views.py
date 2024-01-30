cf9b1c85dc6780dab00337cde2c6a788d54dd892
Gabriel Mateus da Silva <g.silva01@cloud.com>
2024-01-30 20:49:48 -0300

    removido url contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Gabriel Silva',
     })
 
-def contato(request):
-    return HttpResponse('recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
